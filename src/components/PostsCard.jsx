d8d7b6dffb2b403668c2b591fba89530934755ee
altantogoo <82674692+ToKoHuber@users.noreply.github.com>
2024-12-18 23:56:27 +0800

    home update-1

--- src/components/PostsCard.jsx
+++ src/components/PostsCard.jsx
@@ -1,7 +1,7 @@
 const PostsCard = (props) => {
   return (
     <div className="PostsCard">
-      <h2>{props.date}</h2>
+      <h4>{props.date}</h4>
       <h3>{props.title}</h3>
     </div>
   );
---
+++ src/components/SinglePostCard.jsx
@@ -0,0 +1,14 @@
+const SinglePostCard = (props) => {
+  <div className="SinglePostCard">
+    <div className="SinglePostCardImg">
+      <img
+        src="https://dash-api.yld.mn/api/image/28b2bfc0-03b5-41d1-befc-0b4e483c81f8"
+        alt=""
+      />
+    </div>
+    <div className="SinglePostCardText">
+      <h2>Хэвлэлийн мэдээ</h2>
+      <h3>Monkondi брэнд Just Chocolate-г зах зээлд нэвтрүүллээ</h3>
+    </div>
+  </div>;
+};

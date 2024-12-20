bf300004c3f1d284aa127bece1c8437156fca06d
altantogoo <82674692+ToKoHuber@users.noreply.github.com>
2024-12-21 00:19:39 +0800

    newsletter-1

--- src/components/NewsLetterSUb.jsx
+++ src/components/NewsLetterSUb.jsx
@@ -1,12 +1,33 @@
 const NewsLetterSub = () => {
   return (
     <div className="NewsLetterSub">
-      <div className="SubBackgroundContentFramme">
+      <div className="NewsLetterSubFrame">
+        <div className="NewsLetterSubContents">
+          <div className="newsletter-title">NEWSLETTER</div>
+          <div className="newsletter-promise">
+            <h3>
+              Хөрөнгө оруулалт, бизнесийн тоймыг
+              <span className="newsletter-promise-bold-text"> 5 минутад</span>
+            </h3>
+          </div>
+          <div className="newsletter-email-input">
+            <input
+              className="Newsletter-email"
+              type="email"
+              name="email"
+              id=""
+              placeholder="Имэйл хаяг"
+            />
+            <button className="Newsletter-Button">Уншаад үзье</button>
+          </div>
+          <div className="newsletter-desc">
+            <p>
+              Таны шийдвэр гаргалтад зайлшгүй хэрэгтэй мэдээллийг агуулсан имэйл
+              товхимлыг ажлын өдөр бүр, өглөө 07:00 цагт үнэгүй илгээнэ.
+            </p>
+          </div>
+        </div>
         <div></div>
-        <div></div>
-      </div>
-      <div className="SubBackgroundFramme">
-        <img src="https://lemonpress.mn/assets/subscribe-cover.png" alt="" />
       </div>
     </div>
   );
